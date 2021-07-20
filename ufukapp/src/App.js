c046056216aaafab73c63618b1811924b62f32aa
Thandei <67441515+Thandei@users.noreply.github.com>
2021-07-20 13:46:03 +0300

    navbar improved

--- ufukapp/src/App.js
+++ ufukapp/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Topbar from "./components/Topbar/Topbar"
+import {useState} from "react"
  
 
 function App() {
+
+  
+
   return (
-   <Topbar />  
+   <Topbar   />  
  
 
 
--- ufukapp/src/components/Topbar/Topbar.js
+++ ufukapp/src/components/Topbar/Topbar.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import "./Topbar.css"
+import { useState } from 'react';
 
 export default function Topbar() {
+
+    const [barOpen, setBarOpen] = useState(false);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+       
+        setBarOpen(!barOpen);
+    }
+
+
     return (
+        <>
         <div className="top">
             <div className="menu-content">
                 <div className="logo">
@@ -14,12 +26,31 @@ export default function Topbar() {
                     <a href="">Works</a> 
                     <a href="">Contact</a>
                 </div> 
-                <div className="hamburger">
+                <div onClick={handleClick} className="hamburger">
                      <span></span>
                      <span></span>
                      <span></span>
                 </div>
+                
+               
             </div>
         </div>
+       
+        <div style={{left: barOpen ? 0 : -2000+"px"}} className="bar">
+        <div className="bar-content">
+            <div className="bar-links">
+                <a href="">Home</a>
+                <a href="">About</a>
+                <a href="">Works</a> 
+                <a href="">Contact</a>
+                 
+                <a href="" className="rotate" onClick={(e)=>handleClick(e)}>+</a>
+             </div> 
+        </div>
+    </div>
+        
+      
+         
+     </>
     )
 }
